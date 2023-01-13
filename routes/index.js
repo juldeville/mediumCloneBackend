89cc8f345d9f84223a7163b3a31f8f13b8075c7d
Julien Deville <j.deville@protonmail.com>
2023-01-13 19:35:47 +0100

    added more articles to api endpoint

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,25 @@ var router = express.Router();
 //Get articles
 
 router.get('/articles', (req, res) => {
-  fetch('https://api.realworld.io/api/articles')
+  fetch('https://api.realworld.io/api/articles?limit=197&offset=0')
   .then(response => response.json())
   .then(apiData => {
     if(JSON.stringify(apiData.articles) !== '{}') {
       res.json({articles: apiData.articles})
+    } else {
+      res.json({error: 'no data found'})
+    }
+  })
+})
+
+//Get tags
+
+router.get('/PopularTags', (req, res) => {
+  fetch('https://api.realworld.io/api/tags')
+  .then(response => response.json())
+  .then(apiData => {
+    if(JSON.stringify(apiData.tags) !== '{}') {
+      res.json({tags: apiData.tags})
     } else {
       res.json({error: 'no data found'})
     }
